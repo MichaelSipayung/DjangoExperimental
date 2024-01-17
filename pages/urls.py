22013cb015d0ff3fa73dadfb239f50371f3fc0eb
Michael Sipayung <Michaelsipayung123@gmail.com>
2024-01-17 16:14:48 +0700

    extends template

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path #import the path function from django.urls
-from .views import HomePageView #import the homePageView function from views.py
+from .views import HomePageView, AboutPageView #import the homePageView function from views.py
 urlpatterns = [
     #for class based views, we need to use the as_view() method
-    path('', HomePageView.as_view(), name='home') 
+    path('', HomePageView.as_view(), name='home'), 
+    path('about/', AboutPageView.as_view(), name='about'),
 ]
